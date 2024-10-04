1be1d62614b66c9705a59c4374c4cbed11b8bb68
brandondionisio <brandon.dionisio@tufts.edu>
2024-10-04 16:40:27 -0400

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import FirstComponent from '@/components/firstComponent';
+
+function App() {
+
+  return (
+    <div>
+      <FirstComponent></FirstComponent>
+    </div>
+  );
+}
+
+export default App
---
+++ frontend/src/components/firstComponent.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+export default function firstComponent() {
+    return (
+        <div className="text-red-400">this is a test component</div>
+    );
+}
