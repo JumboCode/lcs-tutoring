9e4ca26afac4180396d92e215782fe068dbb5b7f
annewu109 <annewu109@gmail.com>
2024-10-17 21:33:59 -0400

    button now displays weather when clicked

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,26 +1,32 @@
 import { useState } from 'react';
 
 export default function Button() {
-    const [temperature, setTemperature] = useState(0);
+    const [temperature, setTemperature] = useState();
     const [weather, setWeather] = useState();
 
-    async function handleClick() {
-        const response = await fetch("https://api.weather.gov/gridpoints/BOX/69,92/forecast"); 
-        const data = response.json();
-        setTemperature(data.properties.periods[0].temperature);
+    function handleClick() {
         
-        //get api stuff
-        // fetch("https://api.weather.gov/gridpoints/BOX/69,92/forecast")
-        //     .then(response => response.json())
-        //     .then(data => console.log(data));
+        fetch("https://api.weather.gov/gridpoints/BOX/69,92/forecast")
+            .then(response => response.json())
+            .then(data => data.properties.periods[0]) 
+            .then(data => {
+                setWeather(data.shortForecast);
+                setTemperature(data.temperature);
+            });
 
         
     }
 
     return (
-        <button onClick={handleClick}>
-            A button! Yayyyy!
-        </button>
+        <div className="flex flex-col">
+            <button onClick={handleClick}>
+                A button! Yayyyy!
+            </button>
+            <ul>
+                <li>Weather: {weather}</li>
+                <li>Temperature: {temperature} F</li>
+            </ul>
+        </div>
     );
 }
 
