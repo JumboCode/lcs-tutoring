af8853f149c454534ee77cc2a55a698b5b16916f
rgs-234 <rachelgsnyder@gmail.com>
2024-10-17 20:16:20 -0400

    weather api functionality (i.e. best commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import FirstComponent from "@/components/firstComponent";
-//
+import Button from "./components/Button";
+
 function App() {
   return (
     <div>
       <FirstComponent></FirstComponent>
+      <Button/>
     </div>
     //hello this is a test of pushing to github
   );
