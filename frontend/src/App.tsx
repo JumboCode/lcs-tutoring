71bfb4353a9aa748f00eb32779bf903c46aee5e9
brandondionisio <brandon.dionisio@tufts.edu>
2024-10-20 13:19:54 -0400

    Added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,39 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import BrandonButton from "@/components/brandon_button";
 import FirstComponent from "@/components/firstComponent";
-import Button from "./components/Button";
+import SethRachelButton from "./components/seth_rachel_button";
 
 function App() {
   return (
-    <div>
-      <BrandonButton />
-      <FirstComponent></FirstComponent>
-      <Button />
-    </div>
-    //hello this is a test of pushing to github
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <FirstComponent />
+            </div>
+          }
+        ></Route>
+
+        <Route
+          path="/testbuttons"
+          element={
+            <div>
+              <BrandonButton />
+              {/* <SethRachelButton /> */}
+            </div>
+          }
+        ></Route>
+
+        {/* New route to display the next component! */}
+        <Route
+          path="/[new name]" // TODO: Change the path name to match that of your component
+          element={<div>{/* TODO: Include your component here: */}</div>}
+        ></Route>
+      </Routes>
+    </Router>
   );
 }
 
