8fc7ba4b3afb7aff9e3b3eb5bce4456c4d5dcb6d
hkniimi <hunter.niimi33@gmail.com>
2024-10-24 10:24:00 -0400

    Got page up and working, need to change fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,55 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import LcsTutoringIntro from "./components/intro";
+// // import BrandonButton from "@/components/buttonBrandon";
+// // import SethRachelButton from "./components/buttonSethRachel";
+// // import ValentinaCharlieButton from "./components/buttonValentinaCharlie";
+// // import AnneRainierButton from "@/components/buttonAnneRainier";
+// // import ArayHunterButton from "./components/buttonArayHunter";
 
-// import BrandonButton from "@/components/buttonBrandon";
-// import SethRachelButton from "./components/buttonSethRachel";
-// import ValentinaCharlieButton from "./components/buttonValentinaCharlie";
-// import AnneRainierButton from "@/components/buttonAnneRainier";
-// import ArayHunterButton from "./components/buttonArayHunter";
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<div></div>}></Route>
+
+//         <Route
+//           path="/testbuttons"
+//           element={
+//             <div>
+//               {/* <BrandonButton /> */}
+//               {/* <SethRachelButton /> */}
+//               {/* <ValentinaCharlieButton /> */}
+//               {/* <AnneRainierButton /> */}
+//               {/* <ArayHunterButton /> */}
+//             </div>
+//           }
+//         ></Route>
+
+//         {/* New route to display the next component! */}
+//         <Route
+//           path="/intro" // TODO: Change the path name to match that of your component
+//           element={<div>{<LcsTutoringIntro/>}</div>} //{<div>{/* TODO: Include your component here: */}</div>}
+//         ></Route>
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LcsTutoringIntro from "./components/intro";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div></div>}></Route>
+        {/* Redirect to /intro */}
+        <Route path="/" element={<Navigate to="/intro" />} />
 
         <Route
-          path="/testbuttons"
-          element={
-            <div>
-              {/* <BrandonButton /> */}
-              {/* <SethRachelButton /> */}
-              {/* <ValentinaCharlieButton /> */}
-              {/* <AnneRainierButton /> */}
-              {/* <ArayHunterButton /> */}
-            </div>
-          }
-        ></Route>
-
-        {/* New route to display the next component! */}
-        <Route
-          path="/[new name]" // TODO: Change the path name to match that of your component
-          element={<div>{/* TODO: Include your component here: */}</div>}
+          path="/intro"
+          element={<LcsTutoringIntro />}
         ></Route>
       </Routes>
     </Router>
