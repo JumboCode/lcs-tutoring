40529c708f36e211eb9bd93f2afa906a87d5102c
brandondionisio <brandon.dionisio@tufts.edu>
2024-11-03 15:13:09 -0500

    staged more backend changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,35 +1,18 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { eq } from 'drizzle-orm';
-import { usersTable } from './db/schema';
-  
+import { tutorTable } from './db/schema';
+import { or, arrayContains } from 'drizzle-orm';
+
 const db = drizzle(process.env.DATABASE_URL!);
-async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: 'John',
-    age: 30,
-    email: 'john@example.com',
-  };
-  await db.insert(usersTable).values(user);
-  console.log('New user created!')
-  const users = await db.select().from(usersTable);
-  console.log('Getting all users from the database: ', users)
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log('User info updated!')
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log('User deleted!')
+
+/* Filter Tutors by their grade levels
+ * 
+ * Example input: filterTutors([9, 10])
+ *     Should return all tutors that have selected 9th or
+ *     10th graders in their grade preferences
+ * 
+ * You can console.log all the tutors that the query returns to verify
+ * a correct output
+ */
+async function filterTutors(gradeLevels?: number[]) {
 }
-main();
