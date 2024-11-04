d0bf3b7f824e93c18926975b70078cbf21287f66
caitlynwei <caitlyn.wei@tufts.edu>
2024-11-03 21:15:47 -0500

    added feature: can put in 0, 1, or both parameters

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,20 +14,33 @@ const db = drizzle(process.env.DATABASE_URL!);
  * You can console.log all the tutors that the query returns to verify
  * a correct output
  */
+
+
 async function filterTutors(gradeLevels?: number[], subject_pref?: string[]) {
     const query = db.select().from(tutorTable);
 
-    if (subject_pref && gradeLevels) {
+    const conditions: any[] = [];
+
+    if(subject_pref && subject_pref.length > 0){
         const condition_subject = subject_pref.map(subject => arrayContains(tutorTable.subject_pref, [subject]));
+        conditions.push(or(...condition_subject));
+    }
+
+    if (gradeLevels && gradeLevels.length > 0) {
+        
         const condition_grade = gradeLevels.map(grade => arrayContains(tutorTable.grade_level_pref, [grade]));
-        query.where(and(or(...(condition_subject)),or(...(condition_grade))));
+        conditions.push(or(...condition_grade));
+    }
+
+    if (conditions.length > 0) {
+        query.where(and(...conditions));
     }
 
     const tutors = await query;
     console.log(tutors);
 }
 
-/*async function main(subject_pref?: string[]) {
+async function main(subject_pref?: string[]) {
     const query = db.select().from(tutorTable);
 
     if (subject_pref) {
@@ -37,6 +50,6 @@ async function filterTutors(gradeLevels?: number[], subject_pref?: string[]) {
 
     const tutors = await query;
     console.log(tutors);
-}*/
+}
 
-filterTutors([10,11],["Inorganic Chemistry", "American History", "Calculus"]);
+filterTutors(undefined, []);
