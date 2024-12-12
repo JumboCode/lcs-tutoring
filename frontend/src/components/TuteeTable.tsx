240c78f3ed755e9794c6332b82c801c0f944bbcd
brandondionisio <brandon.dionisio@tufts.edu>
2024-12-12 02:59:29 -0500

    added vercel.json

--- frontend/src/components/TuteeTable.tsx
+++ frontend/src/components/TuteeTable.tsx
@@ -31,7 +31,7 @@ export default function TuteeTable() {
         // https://jumbocodegpt.onrender.com/api/add-prompt
         // http://localhost:3000/tutees
         const response = await fetch(
-          "https://jumbocodegpt.onrender.com/api/add-prompt/tutees"
+          "https://jumbocodegpt.onrender.com/api/tutees"
         );
         const data = await response.json();
         const { matchedTutees, unmatchedTutees } = data;
