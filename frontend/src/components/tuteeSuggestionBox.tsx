12a833bbc14fe8b02ab3dc06e85e0dca482e77c0
brandondionisio <brandon.dionisio@tufts.edu>
2025-02-06 13:39:57 -0500

    added hover functionality for emails in match suggestions:

--- frontend/src/components/tuteeSuggestionBox.tsx
+++ frontend/src/components/tuteeSuggestionBox.tsx
@@ -2,7 +2,7 @@
 
 import { tuteeInfo } from "../types";
 import { BsEnvelope } from "react-icons/bs";
-// import { useState } from "react";
+import { useState } from "react";
 // const BG_COLOR = "#fbfbfb";
 
 export default function TuteeSuggestionBox({
@@ -23,6 +23,8 @@ export default function TuteeSuggestionBox({
     special_needs,
     tutoring_mode,
   } = tutee_info;
+
+  const [showPopup, setShowPopup] = useState(false);
   return (
     <div className="border rounded-lg w-full">
       <div className="flex gap-4 items-center ml-5 p-2">
@@ -42,19 +44,30 @@ export default function TuteeSuggestionBox({
           </div>
         </div>
       </div>
-      <div className="py-1 font-interBlack flex flex-row text-gray-500 px-8 bg-[#fbfbfb] justify-start items-center mx-2">
+      <div className="py-1 flex flex-row text-gray-500 px-8 bg-[#fbfbfb] justify-start items-center mx-2">
         <span className="w-1/2">Grade</span>
         <span className="w-1/2">Subject</span>
       </div>
-      <div className="py-2 font-interBlack flex flex-row text-[black] px-8 justify-start items-center mx-2">
+      <div className="py-2 flex flex-row text-[black] px-8 justify-start items-center mx-2">
         <span className="w-1/2">{grade}</span>
-        <span className="w-1/2">{subjects.join(", ")}</span>
+        <p
+          className="w-1/2 overflow-hidden text-ellipsis whitespace-nowrap"
+          onMouseEnter={() => setShowPopup(true)}
+          onMouseLeave={() => setShowPopup(false)}
+        >
+          {subjects.join(", ")}
+        </p>
+        {showPopup && (
+          <div className="absolute mt-[70px] ml-[90px] w-auto p-2 bg-white border border-gray-300 shadow-lg">
+            {subjects.join(", ")}
+          </div>
+        )}
       </div>
-      <div className="py-1 font-interBlack flex flex-row text-gray-500 px-8 bg-[#fbfbfb] justify-start items-center mx-2">
+      <div className="py-1 flex flex-row text-gray-500 px-8 bg-[#fbfbfb] justify-start items-center mx-2">
         <span className="w-1/2">Special Needs</span>
         <span className="w-1/2">Tutoring Mode</span>
       </div>
-      <div className="py-2 font-interBlack flex flex-row text-[black] px-8 justify-start items-center mx-2">
+      <div className="py-2 flex flex-row text-[black] px-8 justify-start items-center mx-2">
         <span className="w-1/2">{special_needs}</span>
         <span className="w-1/2">{tutoring_mode}</span>
       </div>
