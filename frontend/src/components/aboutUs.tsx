ec5fc67d7951137507d8be4d3a6067c97f8ff88e
Brandon Dionisio <145251710+brandondionisio@users.noreply.github.com>
2025-02-06 19:35:26 -0500

    Revert "Delete tutee and tutor"

--- frontend/src/components/aboutUs.tsx
+++ frontend/src/components/aboutUs.tsx
@@ -1,31 +1,26 @@
 "use client";
 import Pin from "../assets/images/homepage_pin.svg";
 
-const AboutUs = () => {
-    return (
-        <div className="p-24 bg-[#FBFAFC]">
-            <div className="flex items-center mb-4">
-            <span className="font-interMedium mr-2" style={{ fontSize: "32px", color: "#1F3A68" }}>
-              ABOUT US
-            </span>
-            <div
-              className="border-b border-[#E0D1ED]"
-              style={{ width: "40px" }}
-            ></div>
-            <img src={Pin} alt="pin" className="w-14 h-14 object-right" />
-            </div>
+export default function AboutUs() {
+  return (
+    <div className="p-12 md:p-24 bg-[#FBFAFC]">
+      <div className="flex items-center mb-4">
+        <span className="font-interMedium mr-2 text-3xl text-[#1F3A68]">
+          ABOUT US
+        </span>
+        <div className="border-b border-[#E0D1ED] w-[40px]"></div>
+        <img src={Pin} alt="pin" className="w-14 h-14 object-right" />
+      </div>
 
-
-            <p className="" style={{ fontSize: "48px", color: "#7D7D7D" }}>
-                We are <span className="text-[#23355E]">LCS Tutoring</span>,
-                a <span className="text-[#23355E]">community service</span> organization 
-                at Tufts devoted to <span className="text-[#23355E]">
-                pairing Jumbos with K-12 students</span> in the 
-                Medford/Somerville areas for free tutoring.
-            </p>
-            
-        </div>
-    )
+      <p className="text-2xl/relaxed md:text-4xl/relaxed text-[#7D7D7D]">
+        We are <span className="text-[#23355E]">LCS Tutoring</span>, a{" "}
+        <span className="text-[#23355E]">community service</span> organization
+        at Tufts devoted to{" "}
+        <span className="text-[#23355E]">
+          pairing Jumbos with K-12 students
+        </span>{" "}
+        in the Medford/Somerville areas for free tutoring.
+      </p>
+    </div>
+  );
 }
-
-export default AboutUs
