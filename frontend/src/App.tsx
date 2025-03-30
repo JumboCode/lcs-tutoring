ea97d5e540e254d5f577538ea3c3cc768c6e3472
brandondionisio <brandon.dionisio@tufts.edu>
2025-03-30 13:26:04 -0400

    reverted clerk stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -31,195 +31,184 @@ import AdminLogin from "./components/adminSignIn";
 import SuccessPage from "./components/SuccessPage";
 import EListForm from "./components/E-ListForm";
 
-import {
-  SignIn,
-  SignedIn,
-  SignInButton,
-  SignedOut,
-  RedirectToSignIn,
-  SignOutButton,
-} from "@clerk/react-router";
-import elephantLogo from "./assets/images/elephant.svg";
+// import {
+//   SignIn,
+//   SignedIn,
+//   SignInButton,
+//   SignedOut,
+//   RedirectToSignIn,
+//   SignOutButton,
+// } from "@clerk/react-router";
+// import elephantLogo from "./assets/images/elephant.svg";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-const CLERK_AFTER_SIGN_IN = import.meta.env.CLERK_AFTER_SIGN_IN;
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+// const CLERK_AFTER_SIGN_IN = import.meta.env.CLERK_AFTER_SIGN_IN;
 
 function App() {
   const [modalShow, setModalShow] = useState(false);
 
   return (
-    <ClerkProvider
-      appearance={{
-        layout: {
-          logoImageUrl: elephantLogo,
-        },
-      }}
-      publishableKey={PUBLISHABLE_KEY}
-      afterSignOutUrl="/"
-      signInUrl="/admin"
-    >
-      <Router>
-        <Routes>
-          {/* Client View */}
-          <Route
-            path="/"
-            element={
-              <div className="flex flex-col">
-                <ScrollToTop />
-                <Header />
-                <HomePage />
-                <Footer />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/testbuttons"
-            element={
-              <div>
-                <BrandonButton />
-                <SethRachelButton />
-                <ValentinaCharlieButton />
-                <AnneRainierButton />
-                <ArayHunterButton />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/team"
-            element={
-              <div className="flex flex-col">
-                <ScrollToTop />
-                <Header />
-                <TeamPage />
-                <EListForm />
-                <Footer />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/tutor-form"
-            element={
-              <div className="flex flex-col">
-                <ScrollToTop />
-                <Header />
-                <TutorForm />
-                <Footer />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/tutee-form"
-            element={
-              <div className="flex flex-col">
-                <ScrollToTop />
-                <Header />
-                <TuteeForm />
-                <Footer />
-              </div>
-            }
-          ></Route>
+    <Router>
+      <Routes>
+        {/* Client View */}
+        <Route
+          path="/"
+          element={
+            <div className="flex flex-col">
+              <ScrollToTop />
+              <Header />
+              <HomePage />
+              <Footer />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/testbuttons"
+          element={
+            <div>
+              <BrandonButton />
+              <SethRachelButton />
+              <ValentinaCharlieButton />
+              <AnneRainierButton />
+              <ArayHunterButton />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/team"
+          element={
+            <div className="flex flex-col">
+              <ScrollToTop />
+              <Header />
+              <TeamPage />
+              <EListForm />
+              <Footer />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/tutor-form"
+          element={
+            <div className="flex flex-col">
+              <ScrollToTop />
+              <Header />
+              <TutorForm />
+              <Footer />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/tutee-form"
+          element={
+            <div className="flex flex-col">
+              <ScrollToTop />
+              <Header />
+              <TuteeForm />
+              <Footer />
+            </div>
+          }
+        ></Route>
 
-          <Route
-            path="/success-page"
-            element={
-              <div className="flex flex-col">
-                <ScrollToTop />
-                <Header />
-                <SuccessPage />
-                <Footer />
-              </div>
-            }
-          ></Route>
+        <Route
+          path="/success-page"
+          element={
+            <div className="flex flex-col">
+              <ScrollToTop />
+              <Header />
+              <SuccessPage />
+              <Footer />
+            </div>
+          }
+        ></Route>
 
-          {/* Admin View WIP */}
-          <Route
-            path="/approved-matches"
-            element={
-              <div>
-                <ApprovedMatches />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/elist-form"
-            element={
-              <div>
-                <EListForm />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/filters"
-            element={
-              <div>
-                <button
-                  className={
-                    "flex flex-row items-center px-4 py-2 bg-[#FFFFFF] border-[#E7E7E7] rounded-lg border-1 text-[#888888]"
-                  }
-                  onClick={() => setModalShow(true)}
-                >
-                  <img className={"mr-2"} src={filtersIcon} />
-                  Filters
-                </button>
-                <FilterModal
-                  show={modalShow}
-                  onHide={() => setModalShow(false)}
-                  onApply={() => {
-                    setModalShow(false);
-                  }}
-                />
-              </div>
-            }
-          ></Route>
+        {/* Admin View WIP */}
+        <Route
+          path="/approved-matches"
+          element={
+            <div>
+              <ApprovedMatches />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/elist-form"
+          element={
+            <div>
+              <EListForm />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/filters"
+          element={
+            <div>
+              <button
+                className={
+                  "flex flex-row items-center px-4 py-2 bg-[#FFFFFF] border-[#E7E7E7] rounded-lg border-1 text-[#888888]"
+                }
+                onClick={() => setModalShow(true)}
+              >
+                <img className={"mr-2"} src={filtersIcon} />
+                Filters
+              </button>
+              <FilterModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                onApply={() => {
+                  setModalShow(false);
+                }}
+              />
+            </div>
+          }
+        ></Route>
 
-          <Route path="/admin" element={<NavigationBar />}></Route>
+        <Route path="/admin" element={<NavigationBar />}></Route>
 
-          <Route
-            path="/tuteetable"
-            element={
-              <div>
-                <TuteeTable />
-              </div>
-            }
-          ></Route>
+        <Route
+          path="/tuteetable"
+          element={
+            <div>
+              <TuteeTable />
+            </div>
+          }
+        ></Route>
 
-          <Route
-            path="/approvedmatchestable"
-            element={
-              <div>
-                <ApprovedMatches />
-              </div>
-            }
-          ></Route>
+        <Route
+          path="/approvedmatchestable"
+          element={
+            <div>
+              <ApprovedMatches />
+            </div>
+          }
+        ></Route>
 
-          <Route
-            path="/tutortable"
-            element={
-              <div>
-                <TutorTable />
-              </div>
-            }
-          ></Route>
+        <Route
+          path="/tutortable"
+          element={
+            <div>
+              <TutorTable />
+            </div>
+          }
+        ></Route>
 
-          <Route
-            path="/demo"
-            element={
-              <div>
-                <Demo />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/adminLogin"
-            element={
-              <div>
-                <AdminLogin />
-              </div>
-            }
-          ></Route>
-        </Routes>
-      </Router>
-    </ClerkProvider>
+        <Route
+          path="/demo"
+          element={
+            <div>
+              <Demo />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/adminLogin"
+          element={
+            <div>
+              <AdminLogin />
+            </div>
+          }
+        ></Route>
+      </Routes>
+    </Router>
   );
 }
 
